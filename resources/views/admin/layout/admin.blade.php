3bbbc393c2d4c04a52652da8d8f505f20db8f72f
pankaj-hexagon <pankaj-hexagon@xendit.co>
2022-07-13 19:19:16 +0530

    Code update

--- resources/views/admin/layout/admin.blade.php
+++ resources/views/admin/layout/admin.blade.php
@@ -206,7 +206,7 @@
                                     <div class="menu-submenu menu-submenu-classic menu-submenu-left">
                                         <ul class="menu-subnav">
                                             <li class="menu-item menu-item-active" aria-haspopup="true">
-                                                <a href="index.html" class="menu-link">
+                                                <a href="{{url()}}" class="menu-link">
 															<span class="svg-icon menu-icon">
 																<!--begin::Svg Icon | path:assets/media/svg/icons/Clothes/Briefcase.svg-->
 																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
